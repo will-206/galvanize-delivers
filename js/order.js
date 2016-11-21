da8d43a7fcef69cda92fdbf0e7f2587ed0bbec59
will-206 <will9_30@yahoo.com>
2016-11-21 01:34:29 -0800

    added order column

---
+++ js/order.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+
+
+
+  // const fields = [];
+  //
+  // fields.push(document.querySelector('label[name="name"]'));
+  // fields.push(document.querySelector('input[name="phone"]'));
+  // fields.push(document.querySelector('input[name="address"]'));
+  // console.log(fields);
+  //
+  // $('#order').on('submit', () => {
+  //   event.preventDefault()
+  //   console.log("submit");
+  //   // const fields = $('input-field');
+  //   // const $required = $('input-field');
+  //   for(const element of fields) {
+  //     // if(element.textContent.length = 0) {
+  //     //   console.log(element.name + "not valid");
+  //     // }
+  //     console.log(element.textContent);
+  //   }
+  // });
+})();
